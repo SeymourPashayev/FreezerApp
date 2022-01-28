493cbd886ca1df8d3da525fd0493e8374dd40440
Seymour Pashayev <gordonsamh@gmail.com>
2022-01-28 15:12:46 +0400

    Initial Commit

---
+++ myCoach/myCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCoachApp.swift
+//  myCoach
+//
+//  Created by Seymour Pashayev on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct myCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
